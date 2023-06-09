ac955d154f8e4b751aa7478fb650b1365310eb3b
Nicholas De Raj <nickderaj@gmail.com>
2023-06-09 20:56:19 +0800

    feat: add hardhat config for smart contract

---
+++ contract/hardhat.config.ts
@@ -0,0 +1,21 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: __dirname + '/.env' });
+
+const ETHERSCAN_KEY = process.env.ETHERSCAN_API_KEY ?? '';
+const ALCHEMY_KEY = process.env.ALCHEMY_API_KEY ?? '';
+const SEPOLIA_KEY = process.env.SEPOLIA_PRIVATE_KEY ?? '';
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.18',
+  etherscan: { apiKey: ETHERSCAN_KEY },
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_KEY}`,
+      accounts: [SEPOLIA_KEY],
+    },
+  },
+};
+
+export default config;
